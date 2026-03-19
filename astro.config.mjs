650f1a253d9e6b8fed5645e6c32e1e76600438b0
viirak <viirak@gmail.com>
2026-03-19 13:07:30 +0700

    build(config): switch to static output
    - Removed Cloudflare adapter
    - Configured static build output
    - Simplifies deployment for static hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 
-import cloudflare from "@astrojs/cloudflare";
 import partytown from "@astrojs/partytown";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mermind.ai",
-
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+	output: "static",
 
 	image: {
 		service: {

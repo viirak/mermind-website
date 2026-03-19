5b1b4de69b979b878fcee483ef6d8017fa43598b
viirak <viirak@gmail.com>
2026-03-19 12:57:43 +0700

    build(config): add Partytown and image service
    - Add Partytown integration
    - Configure Sharp image service
    - Set image quality and formats
    - Define site URL for Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import partytown from "@astrojs/partytown";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    },
+	site: "https://mermind.ai",
 
-    imageService: "cloudflare"
-  })
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
+
+	image: {
+		service: {
+			entrypoint: "@astrojs/image/sharp",
+			config: {
+				quality: 80,
+				format: ["webp", "avif", "jpeg"],
+			},
+		},
+	},
+
+	integrations: [partytown()],
 });

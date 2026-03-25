de5a9c43011a73d491b61382c2a753625e23d96a
viirak <viirak@gmail.com>
2026-03-25 11:37:01 +0700

    chore: moved content config to root

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+	loader: glob({ pattern: "**/*.md", base: "./src/blog-posts" }),
+	schema: z.object({
+		title: z.string(),
+		altTitle: z.string().optional(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		coverImage: z.string().optional(),
+		category: z.string().optional(),
+		draft: z.boolean().optional().default(false),
+		isFeatured: z.boolean().optional().default(false),
+	}),
+});
+
+export const collections = { blog };
